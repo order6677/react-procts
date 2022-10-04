5735a07b3eeb905d575199a4010ccd6762231cc7
awadh <awadh.hassan@student.moringaschool.com>
2022-10-04 09:32:17 +0300

    adds project files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import react from 'react'
+import Navbar from './components/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>you</h1>
+      
+    </div>
+  );
+}
+
+export default App;
