39377e1ae21a749a291ee7064492e0c7cc0ab066
awadh <awadh.hassan@student.moringaschool.com>
2022-10-04 11:58:33 +0300

    addition od style.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Meal from './components/Meal';
-
-
+import "./components/style.css";
 
 function App() {
   return (
--- src/components/Meal.js
+++ src/components/Meal.js
@@ -5,7 +5,7 @@ const Meal = () => {
     <>
       <div className="main">
         <div className="heading">
-          <h1>search food recipe</h1>
+          <h1>Search food recipe</h1>
           <h4>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo 
             ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
              nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
