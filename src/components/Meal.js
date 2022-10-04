460ffd26d5e3bf46ef20b87d2e97613e46821d48
awadh <awadh.hassan@student.moringaschool.com>
2022-10-05 00:22:31 +0300

    add styling to card

--- src/components/Meal.js
+++ src/components/Meal.js
@@ -1,4 +1,5 @@
 import React from "react";
+import MealItem from "./Mealitem"
 
 const Meal = () => {
   return (
@@ -7,17 +8,19 @@ const Meal = () => {
         <div className="heading">
           <h1>Search food recipe</h1>
           <h4>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo 
-            ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
-             nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
-              Nulla consequat massa quis enim. Donec pede justo,
-             fringilla vel, aliquet nec
+            
           </h4>
         </div>
         <div className="searchBox">
             <input type ="search" className ="search-bar" />
 
         </div>
-      </div>
+        <div className="container">
+          <MealItem />
+        
+        </div>
+       
+       </div>
     </>
   );
 };
