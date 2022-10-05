78cb10f334ad0f6f7f2c539e2f95e14618507563
awadh <awadh.hassan@student.moringaschool.com>
2022-10-05 23:48:07 +0300

    adding event handler

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,13 +1,14 @@
 
 import React from 'react'
- const Recipe=()=>{
+ const Recipe=({betIndex})=>{
     const alp =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    let num=0;
     return (
         <>
         {
              alp.map(item=>{
                 return(
-                    <div className="numBox">
+                    <div className="numBox" key={num++}onClick={()=>betIndex(item)}>
                         <h3>{item}</h3>
                     </div>
                 )
