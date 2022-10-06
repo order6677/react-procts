3e9cbd476adea13fb74fd2a573903ad1550ad146
awadh <awadh.hassan@student.moringaschool.com>
2022-10-06 09:01:19 +0300

    adding  recipeinfo component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Meal from './components/Meal';
 import "./components/style.css";
+import{Routes,Route}from "react-router-dom"
 
 function App() {
   return (
     <>
-    <Meal />
+    <Routes>
+    <Route path="/" element={<Meal />}/>
+    </Routes>
     </>
       
   
---
+++ src/components/Recipeinfo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+const Recipeinfo=()=>{
+return(
+    <>
+
+    <h3>recipe information</h3>
+    </>
+)
+}
+
+    
+
+export default Recipeinfo
